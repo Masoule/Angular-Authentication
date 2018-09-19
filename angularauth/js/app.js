ff7db006438f108a712d372b4504372a817cf1b8
salome siavoshi <salomes@buildingiq.com>
2018-09-19 14:22:29 -0700

    adds angular routes

--- angularauth/js/app.js
+++ angularauth/js/app.js
@@ -1,5 +1,20 @@
-var authApp = angular.module('auth',[]);
+var authApp = angular.module('auth',['ngRoute']);
 
-authApp.controller('appController',['$scope', function($scope) {
-  $scope.message = 'hello from outer space';
-}])
+authApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      templateURL:'views/login.html',
+      controller:'AuthController'
+    }).
+    when('/register', {
+      templateURL:'views/register.html',
+      controller:'AuthController'
+    }).
+    when('/success', {
+      templateURL:'views/success.html',
+      controller:'SuccessController'
+    }).
+    otherwise({
+      redirectTo:'/login',
+    });
+}]);
